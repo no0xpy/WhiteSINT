0a712f8d19eaaac3616270d9d0ce42b7feff6c73
no0xpy <271783390+no0xpy@users.noreply.github.com>
2026-04-24 18:14:47 +0200

    vsy pk tu veux jmette un msg de commit

--- tools/Discord/discord-menu.py
+++ tools/Discord/discord-menu.py
@@ -1,2 +1,55 @@
+import platform
+import fade
+from colorama import Fore
+import os
+import sys
+import subprocess
+
+
+path = os.path.dirname(os.path.abspath(__file__))
+if platform.system() == "Windows":
+    clearcmd = "cls"
+else:
+    clearcmd = "clear"
+
+banner = r"""
+██████╗ ██╗███████╗ ██████╗ ██████╗ ██████╗ ██████╗ 
+██╔══██╗██║██╔════╝██╔════╝██╔═══██╗██╔══██╗██╔══██╗
+██║  ██║██║███████╗██║     ██║   ██║██████╔╝██║  ██║
+██║  ██║██║╚════██║██║     ██║   ██║██╔══██╗██║  ██║
+██████╔╝██║███████║╚██████╗╚██████╔╝██║  ██║██████╔╝
+╚═════╝ ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⠙⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣷⣶⣤⣄⣈⣉⣉⣉⣉⣉⣉⣉⣁⣤⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⣠⣶⣾⡏⢀⡈⠛⠻⠿⢿⣿⣿⣿⣿⣿⠿⠿⠟⠛⢁⠀⢶⣤⣀⠀⠀⠀
+⠀⢠⣿⣿⣿⣿⡇⠸⣿⣿⣶⣶⣤⣤⣤⣤⣤⣤⣤⣶⣶⣿⡿⠂⣸⣿⣿⣷⡄⠀
+⠀⢸⣿⣿⣿⣿⣿⣦⣄⡉⠛⠛⠛⠿⠿⠿⠿⠛⠛⠛⢉⣁⣤⣾⣿⣿⣿⣿⡷⠀
+⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀
+⠀⠀⠀⠀⠈⠙⠛⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠛⠛⠉⠁⠀⠀⠀⠀
+"""
+
+print(fade.water(banner))
+print(Fore.RED + """[!] This tool is intended for legal, moral and ethical use. The developers are not responsible for how you use it.
+""")
+print(Fore.YELLOW + "1 > Webhook Lookup     2 > Webhook Sender     3 > Webhook Remover")
+choice = input(Fore.MAGENTA + "What do you want to do ? ")
+
+if choice == "1":
+ os.system(clearcmd)
+ subprocess.run([sys.executable, f'{path}/webhook-lookup.py'])
+elif choice == "2":
+ os.system(clearcmd)
+ subprocess.run([sys.executable, f'{path}/webhook-sender.py'])
+elif choice == "3":
+ os.system(clearcmd)
+ subprocess.run([sys.executable, f'{path}/webhook-remover.py'])
+else:
+ os.system(clearcmd)
+ subprocess.run([sys.executable, f'{path}/../../menu.py'])
 
 
