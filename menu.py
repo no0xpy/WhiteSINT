3ff06e6d59cc81a6109bec61f43f341c9cd32d13
no0xpy <271783390+no0xpy@users.noreply.github.com>
2026-04-25 17:18:16 +0200

    Added subdomains finder, please report issues btw

--- menu.py
+++ menu.py
@@ -38,16 +38,17 @@ print(fade.water(banner))
 print(Fore.RED + """[!] This tool is intended for legal, moral and ethical use. The developers are not responsible for how you use it.
 """)
 print(Fore.YELLOW + """
-1 > IP Lookup     
-2 > WHOIS Lookup     
-3 > Phone Lookup     
-4 > Ports Scanner     
-5 > Identity Generator     
-6 > Password Generator     
-7 > Multiple Searcher     
-8 > Discord Tools     
-9 > Roblox Lookup (ID)   
-10 > Breach Lookup (Email)""")
+1 > IP Lookup\t 11 > Subdomains Searcher  
+2 > WHOIS Lookup\t       
+3 > Phone Lookup\t     
+4 > Ports Scanner\t     
+5 > Identity Generator\t     
+6 > Password Generator\t     
+7 > Multiple Searcher\t     
+8 > Discord Tools\t     
+9 > Roblox Lookup (ID)\t   
+10 > Breach Lookup (Email)\t
+""")
 choice = input(Fore.MAGENTA + "What do you want to do ? ")
 
 if choice == "1":
@@ -80,6 +81,9 @@ elif choice == "9":
 elif choice == "10":
  os.system(clearcmd)
  subprocess.run([sys.executable, f'{path}/tools/breach-lookup.py'])
+elif choice == "11":
+ os.system(clearcmd)
+ subprocess.run([sys.executable, f'{path}/tools/subdomains-searcher.py'])
 else:
  os.system(clearcmd)
  subprocess.run([sys.executable, f'{path}/menu.py'])
---
+++ tools/subdomains-searcher.py
@@ -0,0 +1,59 @@
+import platform
+import fade
+from colorama import Fore
+import os
+import sys
+import subprocess
+import subbrute
+
+path = os.path.dirname(os.path.abspath(__file__))
+if platform.system() == "Windows":
+    clearcmd = "cls"
+else:
+    clearcmd = "clear"
+
+banner = r"""
+███████╗██╗   ██╗██████╗ ██████╗  ██████╗ ███╗   ███╗ █████╗ ██╗███╗   ██╗███████╗    ███████╗███████╗ █████╗ ██████╗  ██████╗██╗  ██╗███████╗██████╗ 
+██╔════╝██║   ██║██╔══██╗██╔══██╗██╔═══██╗████╗ ████║██╔══██╗██║████╗  ██║██╔════╝    ██╔════╝██╔════╝██╔══██╗██╔══██╗██╔════╝██║  ██║██╔════╝██╔══██╗
+███████╗██║   ██║██████╔╝██║  ██║██║   ██║██╔████╔██║███████║██║██╔██╗ ██║███████╗    ███████╗█████╗  ███████║██████╔╝██║     ███████║█████╗  ██████╔╝
+╚════██║██║   ██║██╔══██╗██║  ██║██║   ██║██║╚██╔╝██║██╔══██║██║██║╚██╗██║╚════██║    ╚════██║██╔══╝  ██╔══██║██╔══██╗██║     ██╔══██║██╔══╝  ██╔══██╗
+███████║╚██████╔╝██████╔╝██████╔╝╚██████╔╝██║ ╚═╝ ██║██║  ██║██║██║ ╚████║███████║    ███████║███████╗██║  ██║██║  ██║╚██████╗██║  ██║███████╗██║  ██║
+╚══════╝ ╚═════╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝    ╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝                                                                                                                                                                                                                  
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⠙⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣷⣶⣤⣄⣈⣉⣉⣉⣉⣉⣉⣉⣁⣤⡄⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢀⣠⣶⣾⡏⢀⡈⠛⠻⠿⢿⣿⣿⣿⣿⣿⠿⠿⠟⠛⢁⠀⢶⣤⣀⠀⠀⠀
+⠀⢠⣿⣿⣿⣿⡇⠸⣿⣿⣶⣶⣤⣤⣤⣤⣤⣤⣤⣶⣶⣿⡿⠂⣸⣿⣿⣷⡄⠀
+⠀⢸⣿⣿⣿⣿⣿⣦⣄⡉⠛⠛⠛⠿⠿⠿⠿⠛⠛⠛⢉⣁⣤⣾⣿⣿⣿⣿⡷⠀
+⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⡿⠛⠁⠀
+⠀⠀⠀⠀⠈⠙⠛⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠛⠛⠉⠁⠀⠀⠀⠀
+"""
+
+print(fade.water(banner))
+print(Fore.RED + """[!] This tool is intended for legal, moral and ethical use. The developers are not responsible for how you use it.
+""")
+try:
+ target = input(Fore.MAGENTA + "Domain > ")
+ wordlist = subbrute.load(f"{path}/../wordlists/subdomains-list.txt")
+ 
+ def brute(subdomain, ip, status):
+    if status == "valid":
+        print(f"Found Subdomain: {subdomain} IP: {ip}")
+    else:
+        print(f"Not Found: {subdomain}")
+ results = subbrute.scan(target, wordlist, callback=brute)
+ print(Fore.GREEN + results)
+except Exception as e:
+ print("[I] Error:", e)
+
+choice = input(Fore.YELLOW + "[?] Do you want to retry ? (Y/n) ")
+if choice == "y" or choice =="Y":
+ os.system(clearcmd)
+ subprocess.run([sys.executable, f'{path}/subdomains-searcher.py'])
+else:
+ os.system(clearcmd)
+ subprocess.run([sys.executable, f'{path}/../menu.py'])
